6b71696c96440168c20ba0f0edbc852de3cb75af
Sushant Ahuja <sushant.ahuja@highwirepress.com>
2026-02-10 08:13:11 +0530

    Time: 2128 ms (14.47%), Space: 473.3 MB (64.57%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int longestBalanced(vector<int>& nums) {
         int n=nums.size(),ans=0;
-        //tc->n*n
+        //tc->n*n*logn
         for(int i=0;i<n;i++) {
             set<int>e,o;
             for(int j=i;j<n;j++) {
