45fce8c592411d5a68166bbd02b909e54d44571f
Sushant Ahuja <sushant.ahuja@highwirepress.com>
2026-02-18 10:15:22 +0530

    Time: 1 ms (32.14%), Space: 10.2 MB (5.17%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        unordered_map<char,int>lastIndex;
+        int n=s.size();
+        for(int i=0;i<n;i++) {
+            lastIndex[s[i]]=i;
+        }
+        stack<char>st;
+        unordered_set<char>seen;
+        for(int i=0;i<n;i++) {
+            char curr=s[i];
+            if(seen.count(curr)) continue;
+            if(st.empty() || curr>st.top() && (!seen.count(curr))) {
+                st.push(s[i]);
+                seen.insert(curr);
+            }
+            else {
+                while(!st.empty() && lastIndex[st.top()]>i && st.top()>curr) {
+                    seen.erase(st.top());
+                    st.pop();
+                }
+                st.push(curr);
+                seen.insert(curr);
+            }
+        }
+        string ans="";
+        while(!st.empty()) {
+            char curr=st.top();
+            st.pop();
+            ans+=curr;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+    string removeDuplicateLetters(string s) {
+        return smallestSubsequence(s);
+    }
+};
