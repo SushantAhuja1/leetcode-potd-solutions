3f1690e1b09498961722994dafb98b4ad678f50e
Sushant Ahuja <sushant.ahuja@highwirepress.com>
2026-06-14 07:13:20 +0530

    Time: 11 ms (5.07%), Space: 22.9 MB (5.05%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
     int n;
-    unordered_map<int,vector<int>>adj;
-    vector<int>color;
-    bool dfs(int node,int c,int parent) {
-        color[node]=c;
-        for(auto &nbr:adj[node]) {
-            if(nbr==parent) continue;
-            if(color[nbr]==color[node]) return false;
-            if(color[nbr]==-1) {
-                if(!dfs(nbr,!c,node)) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+    unordered_map<int, vector<int>> adj;
+    vector<int> color;
     bool isBipartite(vector<vector<int>>& graph) {
-        n=graph.size();
-        color.resize(n,-1);
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<graph[i].size();j++) {
+        n = graph.size();
+        color.resize(n, -1);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < graph[i].size(); j++) {
                 adj[i].push_back(graph[i][j]);
                 adj[graph[i][j]].push_back(i);
             }
         }
-        for(int i=0;i<n;i++) {
-            if(color[i]==-1) {
-                if(!dfs(i,0,-1)) return false;
+        queue<pair<int, int>> q;
+        for (int i = 0; i < n; i++) {
+            if (color[i] == -1) {
+                q.push({i, 0});
+                color[i] = 0;
+                while (!q.empty()) {
+                    int node = q.front().first;
+                    int c = q.front().second;
+                    q.pop();
+                    for (auto& nbr : adj[node]) {
+                        if (color[nbr] == color[node])
+                            return false;
+                        else if (color[nbr] == -1) {
+                            color[nbr] = !c;
+                            q.push({nbr, !c});
+                        } else {
+                            continue;
+                        }
+                    }
+                }
             }
         }
+
         return true;
     }
 };
